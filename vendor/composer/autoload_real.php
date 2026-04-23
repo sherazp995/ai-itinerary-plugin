c142a5377e9ece8fb14cf57c4fcc76dac2494e8a
Sheraz <sherazp995@gmail.com>
2026-04-23 10:17:35 +0500

    remove vendor packages

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInit649adad2cfd458bf7c39b17587712cef
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit649adad2cfd458bf7c39b17587712cef::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
